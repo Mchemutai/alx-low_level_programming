76c5076e229fcf832095eebc4f61391bd18c6368
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-02-16 22:26:38 -0800

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main prints "Programming is like building a multilingual 
+ * main-uses the function puts to print  "Programming is like building a multilingual 
  * puzzle,followed by a new line."
  * Returns:0(Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
