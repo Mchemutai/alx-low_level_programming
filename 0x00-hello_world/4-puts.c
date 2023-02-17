1415f5f87a6f88cc8d62fa1add4f75d61ca7550c
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-02-16 18:47:11 -0800

    Add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * *main prints "Programming is like building a multilingual 
- * puzzle, followed by a new line"
- * Returns 0
+ * puzzle, followed by a new line."
+ * Returns: Always 0.
  *
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 
 }
