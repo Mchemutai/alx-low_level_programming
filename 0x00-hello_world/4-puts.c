d51a21970a2134091652d71e2a39c3b6df12b4e8
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-02-16 22:18:09 -0800

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
  * main prints "Programming is like building a multilingual 
- * puzzle, followed by a new line."
- * Returns: Always 0
+ * puzzle,followed by a new line."
+ * Returns:0(Success)
  */
 int main(void)
 {
