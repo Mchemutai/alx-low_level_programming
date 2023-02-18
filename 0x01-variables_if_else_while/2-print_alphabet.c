3cec4c8a7901ff14447d7d82a9ca5300278cadcc
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-02-18 05:54:10 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - prints alphabets in lower case
+*
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+char Alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int a;
+for (a = 0; a < 26; a++)
+{
+putchar (Alp[a]);
+putchar ('\n');
+}
+return (0);
+}
