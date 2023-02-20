f7d957b3c3f9d5ed966fca4640de4b36664f6552
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-02-19 23:52:44 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,7 +19,7 @@ putchar((num1 % 10) + '0');
 putchar((num2 % 10) + '0');
 putchar((num3 % 10) + '0');
 if (num1 == 7 && num2 == 8 && num3 == 9)
-continue;			
+continue;
 putchar(',');
 putchar(' ');
 }
