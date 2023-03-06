9f68760c79273bc10aa8ee147106982f152a5482
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-03-06 13:37:47 -0800

    update git

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * more_numbers - entry point
@@ -8,7 +9,7 @@
  * Return: void
  */
 
-void more_numbers(void)
+int main (void)
 {
 int number, tens, units, row;
 for (row = 1; row <= 10; row++)
@@ -18,9 +19,10 @@ for (number = 0; number <= 14; number++)
 tens = number / 10;
 units = number % 10;
 if (number > 9)
-_putchar(tens + '0');
-_putchar(units + '0');
+putchar(tens + '0');
+putchar(units + '0');
 }
-_putchar('\n');
+putchar('\n');
 }
+return (0);
 }
