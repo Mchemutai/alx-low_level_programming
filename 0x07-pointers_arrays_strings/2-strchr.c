3647c5d943508858920204e6b793a16843395945
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-03-06 14:00:09 -0800

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strchr - a function that identifies a character in a string
+ * @s: string to be searched
+ * @c: character to find
+ *
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int m;
+
+	for (m = 0; s[m] != '\0'; m++)
+	{
+		if (s[m] == c)
+		{
+			return (s + m);
+		}
+	}
+	if (s[m] == c)
+	{
+		return (s + m);
+	}
+	return (0);
+}
