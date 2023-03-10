1d603e60d4e4ecb6e088fa98e41215e4ab0b580a
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-03-10 13:40:40 -0800

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_memcpy -   copy number bytes from adress "from" to adress "to"
+ *@dest: content is to be copied
+ *@src: Source of data to be copied
+ *@n: bytes of the memory
+ *Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	unsigned int k;
+
+	k = 0;
+	while (k < n)  /*Delacring WHILE*/
+	{
+		*(dest + k) = *(src + k);
+		k++;
+	}
+
+	return (dest);
+}
