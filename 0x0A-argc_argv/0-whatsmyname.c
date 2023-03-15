b3226ddf8e597a5174e41888e00a2c5087fbf2b6
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-03-15 10:01:10 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main function - Program to Print the Program Name followed by a new line
+*Description: prints the name of the program
+ * @argc: number of command line arguements
+ * @argv: an array of command line arguements
+ * Return: 0
+*/
+
+int  main(int argc, char *argv[]) 
+{
+printf("%s\n", argv[argc * 0]);
+return (0);
+}
