a3bbe62f36070a200190285b89b272ee1205a724
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-03-16 16:19:18 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
