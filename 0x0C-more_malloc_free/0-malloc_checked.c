be13232d42c45ae3173acf8a3899ecf3c3fe7cdf
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-03-16 16:04:48 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
 /**
  * malloc_checked-> checking malloc
  * @a: param a
  */
 void *malloc_checked(unsigned int a)
 {
-    void *p;
-
-    p = malloc(a);
-    if (p == NULL)
-        exit(98);
-    return (p);
+void *p;
+p = malloc(a);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
 }
