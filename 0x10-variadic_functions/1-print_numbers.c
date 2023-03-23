7b493307e1d4185f1edde199060d640fe493fdd8
Mchemutai <51442908+Mchemutai@users.noreply.github.com>
2023-03-23 11:40:02 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,7 +6,7 @@
 *print_numbers - function  to print numbers followed by new line
 *
 *@n:argument
-*@*separator: function argument
+*@separator: string to be printed between lines
 *Return  numbers
 */
 
@@ -18,7 +18,7 @@ va_start(args, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(args, int));
-if (i <(n - 1) && separator != NULL)
+if (i < (n - 1) && separator != NULL)
 {
 printf("%s", separator);
 }
